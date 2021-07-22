6d6f241f4b1000e47f96ed36deb8ca2bf47ffd3d
Felipe Camargo Machado <felipe.camargo@gmail.com>
2021-07-22 20:58:09 +0200

    Proyecto en la rama main

---
+++ src/main/java/com/example/clasespringtransaction/ClaseSpringTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.clasespringtransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaseSpringTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaseSpringTransactionApplication.class, args);
+    }
+
+}
